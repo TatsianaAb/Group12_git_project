33f6cb4ca79515b48620e62a932eeaffa6a8a0ec
Luis Valadez <valopezla2sc@gmail.com>
2022-08-30 19:23:55 -0500

    made loop

---
+++ src/Features/luis/loop_me.java
@@ -0,0 +1,13 @@
+package Features.luis;
+
+public class loop_me {
+    public static void main(String[] args) {
+
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("1");
+        }
+
+
+    }
+}
